80e38e0b8feeb7a221821e7730d672d89b1fd691
jevdossantos <jeversos.dossantos@mercadolivre.com>
2025-06-04 17:50:04 -0300

    Feat: Classes POO - 0528

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,36 @@ public class Main {
         System.out.println("Atendente 2: " + atendente2.getNomeUsuario() + " (Matrícula: " + atendente2.getNumeroMatricula() + ")");
         System.out.println("\n");
 
-        BalcaoAtendimento balcao1 = new BalcaoAtendimento(101, atendente1, "Eletrônicos Center");
+        BalcaoAtendimento balcao1 = new BalcaoAtendimento(101, atendente1, "Center Pop");
         balcao1.iniciarAtendimento();
         balcao1.iniciarAtendimento();
         System.out.println("--- Balcão de Atendimento Criado ---");
         System.out.println("Balcão 1: Atendente: " + balcao1.getAtendente().getNomeUsuario() + ", Loja: " + balcao1.getNomeDaLoja());
         System.out.println("\n");
 
+        Chamados chamado1 = new Chamados("Luana Barros", atendente1, "MacBook Pro", EstadoDoChamado.EM_ATENDIMENTO);
+        Chamados chamado2 = new Chamados("Dandara Santos", atendente2, "Subwooffer JBL", EstadoDoChamado.ABERTO);
+        Chamados chamado3 = new Chamados("Raul Gomes", atendente1, "Monitor Samsung 32", EstadoDoChamado.FECHADO);
+
+        System.out.println("--- Chamados Criados ---");
+        System.out.println("Chamado 1: Cliente: " + chamado1.getNomeDoCliente() + ", Atendente: " + chamado1.getAtendente().getNomeUsuario() + ", Produto: " + chamado1.getNomeDoProduto() + ", Estado: " + chamado1.getEstado());
+        System.out.println("Chamado 2: Cliente: " + chamado2.getNomeDoCliente() + ", Atendente: " + chamado2.getAtendente().getNomeUsuario() + ", Produto: " + chamado2.getNomeDoProduto() + ", Estado: " + chamado2.getEstado());
+        System.out.println("Chamado 3: Cliente: " + chamado3.getNomeDoCliente() + ", Atendente: " + chamado3.getAtendente().getNomeUsuario() + ", Produto: " + chamado3.getNomeDoProduto() + ", Estado: " + chamado3.getEstado());
+        System.out.println("\n");
+
+        // 4. Geração de Relatórios
+        System.out.println("--- Relatórios ---");
+
+        // Relatórios de Chamados
+        System.out.println("--- Relatórios de Chamados ---");
+        System.out.println("Relatório Chamado 1: " + chamado1.geraRelatorio());
+        System.out.println("Relatório Chamado 2: " + chamado2.geraRelatorio());
+        System.out.println("Relatório Chamado 3: " + chamado3.geraRelatorio());
+        System.out.println("\n");
+
+        // Relatório de Balcão de Atendimento
+        System.out.println("--- Relatório de Balcão de Atendimento ---");
+        System.out.println("Relatório Balcão 1: " + balcao1.geraRelatorio());
     }
 }
+
